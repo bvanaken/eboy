7281e76ba779b59763428c11f40a1ae101a0572f
KReguieg <Khaled.Reguieg@gmail.com>
2016-11-20 06:22:37 +0100

    [CHANGED] Category to Tag and model.

--- src/main/java/com/eboy/mv/model/Recognition.java
+++ src/main/java/com/eboy/mv/model/Recognition.java
@@ -2,13 +2,11 @@ package com.eboy.mv.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import java.util.List;
-
 /**
  * Created by root1 on 20/11/16.
  */
 public class Recognition {
-    public Category[] categories;
+    public Tag[] tags;
     public String requestId;
 
     @JsonProperty("metadata")
@@ -17,8 +15,8 @@ public class Recognition {
     public Recognition() {
     }
 
-    public Recognition(Category[] categories, String requestId, MetaData metaData) {
-        this.categories = categories;
+    public Recognition(Tag[] tags, String requestId, MetaData metaData) {
+        this.tags = tags;
         this.requestId = requestId;
         this.metaData = metaData;
     }
